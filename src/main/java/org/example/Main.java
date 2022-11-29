525e3d97093010ccdf0f800fd1fb6dfccca274df
Vitaly Karpii <vitalykarpii@gmail.com>
2022-11-29 16:30:13 +0200

    Add Main.java

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.example.app.App;
+
+/**
+ * TODO: кожен тип заюзати . Лижні курорти тема.(Лижі, курортні доміки) (Чейн/стратегія вивести інфу)
+ * Сервіc - ціну виводити РАЗОМ.
+ * Всі курорти і тд, створюються через породжуючий патерн.
+ * Знайти загальну ціну за всі послуги
+ */
+public class Main {
+    public static void main(String[] args) {
+        App app = new App();
+        app.start();
+    }
+}
